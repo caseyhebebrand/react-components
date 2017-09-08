2714f73e7cc030df346824c515f9e965f8f8bce5
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-09-08 12:19:07 -0700

    dynamically render list items

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-// TODO
+
+// class GroceryListItem extends React.component {
+//   render() {
+//     return(
+//       <li>this.props.todo</li>
+//     )
+//   }
+// }
+//
+// class GroceryList extends React.component {
+//  render() {
+//    return(
+//      <ul>
+//       {props.groceryItems.map(todo =>
+//         <GroceryListItem todo={todo} />
+//       )}
+//       </ul>
+//    );
+//  }
+// }
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList groceryItems={['chips', 'coffee']}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryItems.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+)
+
+var GroceryListItem = (props) => (
+  <li>{props.item}</li>
+)
+
+ReactDOM.render(<App/>, document.getElementById("app"));
